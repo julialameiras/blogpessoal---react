3e179bab7fba40c829b2d028a8e6e274184079cc
julia <julialameiras@gmail.com>
2024-08-21 12:10:35 -0300

    Componente Home - CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./pages/home/Home"
+
+function App() {
+//código typescript
+
+  return (
+    //código tsx = html + css
+    <>
+    {/* <h1>Turma Javascript 05</h1>
+    <h2>React é divertido!</h2>*/}
+    <Home />
+    </>
+  )
+}
+
+export default App
