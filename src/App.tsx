c055eb257c4769b512bb62cb72ad53424ceb8891
julia <julialameiras@gmail.com>
2024-08-26 09:26:01 -0300

    :lipstick: feat: Estilização CSS das Páginas Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
+
 
 function App() {
-//código typescript
+
 
   return (
-    //código tsx = html + css
     <>
-    {/* <h1>Turma Javascript 05</h1>
-    <h2>React é divertido!</h2>*/}
+
+
     <BrowserRouter>
     <Navbar />
     <div className="min-h-[80h]">
     <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/" element={<Home />} />
+    <Route path="/" element={<Cadastro />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cadastro" element={<Cadastro />} />
+    <Route path="/home" element={<Home />} />
     </Routes>
     </div>
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Navbar() {
+
+
+
   return (
     <>
     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
     <div className="container flex justify-between text-lg">
-            <Link to='/home' className="text-2x1 font-bold"> Blog Pessoal</Link>
+    <div className='text-2x1 font-bold uppercase'>Blog Pessoal</div>
 
             <div className='flex gap-4'>
+            <Link to='/login' className='hover:underline'> Login</Link>
+            <Link to='/home' className='hover:underline'> Home</Link>
               <div className='hover:underline'>Postagens</div>
               <div className='hover:underline'>Temas</div>
               <div className='hover:underline'>Cadastrar tema</div>
